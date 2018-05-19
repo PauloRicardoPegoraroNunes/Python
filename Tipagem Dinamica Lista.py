f27a4f0ab0c737ee9c44495c70d70a5d973c59bc
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2018-05-19 17:06:27 -0300

    Lista

---
+++ Tipagem Dinamica Lista.py
@@ -0,0 +1,8 @@
+#uma lista de instrumentos musicas
+instrumentos = ['Baixo', 'Bateria', 'Guitara']
+
+#Para cada nome na lista de instrumentos
+for instrumento in instrumentos:
+    
+    #mostre o nome do instrumento musical
+    print (instrumento) 
