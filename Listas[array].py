cec77ba909d965e08990abba57200057be8bb60c
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2018-05-19 17:04:29 -0300

    Lista

---
+++ Listas[array].py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 16:37:17 2018
+
+@author: paulo
+"""
+
+#simples
+lista = ['baixo','violão','bateria']
+print(lista)
+
+
+
+#tuplas não podem ser alteradas 
+tupla = ('a','b','c','d','e','f','g','h','i')
+print(tupla)
+
+
+
